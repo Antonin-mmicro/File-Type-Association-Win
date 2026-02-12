090715807a7cd47f94de088e89778ccfcb5f49c7
Antonin-mmicro <antonin@mmicro.fr>
2026-02-12 09:39:04 +0100

    Maj ucpd

--- UCPD_HashRotation/UCPD.ps1
+++ UCPD_HashRotation/UCPD.ps1
@@ -2,6 +2,8 @@ try {
     Set-Service -Name UCPD -StartupType Disabled
     Disable-ScheduledTask -TaskName "\Microsoft\Windows\AppxDeploymentClient\UCPD velocity"
     Write-Host "Le service UCPD a été désactivé et la tâche planifiée associée a été désactivée."
+
+    Write-Host "Un reboot est nécessaire"
 }
 catch {
     Write-Host "Le service UCPD n'existe pas ou une erreur est survenue : $_"
